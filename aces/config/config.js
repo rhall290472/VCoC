3a11ba61ad815d0195c9f4b57e47509f9437d71d
Richard Hall <rhall290472@gmail.com>
2025-06-18 14:11:36 -0600

    More updates

--- aces/config/config.js
+++ aces/config/config.js
@@ -2,3 +2,9 @@
 export const API_KEY = 'AIzaSyBcU - XdpJ1rmLpKoivnnF1LdFNmmxR - E1U';
 export const SPREADSHEET_ID = '1QyGDhmzaFUFBO0FnljN9yPix17UbV4GqkjC31cQGiJM';
 export const RANGE = 'WebSiteData!A1:B100';
+
+export const DAY_1_SCRATCH_FORM_ID = '1lYm3vo0DBg7Ux6G0OPEOalMVjpIdhtaHY5EsutnGjxE';
+export const DAY_2_SCRATCH_FORM_ID = '';
+export const DAY_3_SCRATCH_FORM_ID = '';
+export const DAY_4_SCRATCH_FORM_ID = '';
+export const DAY_5_SCRATCH_FORM_ID = '';
