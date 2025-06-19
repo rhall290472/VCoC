f3b6522d376a0e730bc6ab6b985c8ab356c2ffd3
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2025-06-18 22:25:01 -0600

    Forms are workign

--- aces/summerinvite/config.js
+++ aces/summerinvite/config.js
@@ -6,8 +6,9 @@ export const API_KEY = 'AIzaSyBcU - XdpJ1rmLpKoivnnF1LdFNmmxR - E1U';
 export const SPREADSHEET_ID = '1QyGDhmzaFUFBO0FnljN9yPix17UbV4GqkjC31cQGiJM';
 export const RANGE = 'WebSiteData!A1:B100';
 
-export const DAY_1_SCRATCH_FORM_ID = '1lYm3vo0DBg7Ux6G0OPEOalMVjpIdhtaHY5EsutnGjxE';
-export const DAY_2_SCRATCH_FORM_ID = '1845FOyKXfbFzNY73b9Laa54so4_kBsTdOTI_IIPWPDo';
-export const DAY_3_SCRATCH_FORM_ID = '';
-export const DAY_4_SCRATCH_FORM_ID = '';
-export const DAY_5_SCRATCH_FORM_ID = '';
+export const PRELIM_SCRATCH_FORM_ID = '1FAIpQLSdFeQfRYxpp4B8svzxIyzMlY3aPZur1qnDJikV2786haZCP1g';
+export const DAY_1_SCRATCH_FORM_ID  = '1FAIpQLScRNg0qtXaqVVokhmEt8XzUvcQVvo5Z1eNnoFRMoy1ZDTjFbQ';
+export const DAY_2_SCRATCH_FORM_ID  = '1FAIpQLScyQH6ZHDHD-ngmQiN90ydCbd4pWbESOISuo75Tdrdp15iWnQ';
+export const DAY_3_SCRATCH_FORM_ID  = '';
+export const DAY_4_SCRATCH_FORM_ID  = '';
+export const DAY_5_SCRATCH_FORM_ID  = '';
